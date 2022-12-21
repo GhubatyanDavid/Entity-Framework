a5cd5a4138c72043e6407b189118c85f72480f51
David Ghubatyan <79662244+GhubatyanDavid@users.noreply.github.com>
2022-12-21 17:20:56 +0400

    10 Solutions

--- ComputerDatabase/ConsoleApp1/Program.cs
+++ ComputerDatabase/ConsoleApp1/Program.cs
@@ -60,26 +60,59 @@ class Programm
             //}
             //6 /*Для каждого производителя, выпускающего ПК-блокноты c объёмом жесткого диска не менее 10 Гбайт, найти скорости таких ПК-блокнотов. Вывод: производитель, скорость.*/
 
-            var laptops = from Laptop in db.Laptop
-                          join model in db.Product on Laptop.Model equals model.Model
-                          select new
-                          {
-                              Maker = model.Maker,
-                              Speed = Laptop.Speed,
+            //var laptops = from Laptop in db.Laptop
+            //              join model in db.Product on Laptop.Model equals model.Model
+            //              select new
+            //              {
+            //                  Maker = model.Maker,
+            //                  Speed = Laptop.Speed,
 
-                          };
-            foreach (var laptop in laptops)
-            {
-                Console.WriteLine(laptop.Maker, laptop.Speed);
-            }
+            //              };
+            //foreach (var laptop in laptops)
+            //{
+            //    Console.WriteLine(laptop.Maker, laptop.Speed);
+            //}
 
+            //7 /*Найдите номера моделей и цены всех имеющихся в продаже продуктов (любого типа) производителя B (латинская буква).*/
+            //var product = from Product in db.Product
+            //              join pc in db.Pc on product.Model equals pc.Model
+            //              .Where Product.maker = "B"
+            //             .Union(db.Laptop)
+            //              join laptop in db.Laptop on product.Model equals laptop.Model
+            //              .Where Product.maker == "B"
+            //              .Union(db.Printer)
+            //              join printer in db.Printer on product.Model equals printer.Model
+            //              .Where Product.maker == "B";
 
+            //              select new
+            //              {
+            //                  pcModel = pc.model,
+            //                  pcPrice = pc.price,
+            //                  laptopModel = laptop.model,
+            //                  latopPrice = laptop.price,
+            //                  printerModel = printer.model,
+            //                  printerPrice = printer.price,
+            //              };
 
+            //8 /*Найдите производителя, выпускающего ПК, но не ПК-блокноты.*/
+            //var firstProducts = db.Product.Where(firstProducts => firstProducts.Type == "PC")
+            //var secondProducts=db.Product.Where(firstProducts=>firstProducts.Type == "Laptop")
+            //var except = firstproducts.Except(secondProducts).ToList();
 
+            //9 /*Найдите производителей ПК с процессором не менее 450 Мгц. Вывести: Maker */
+            //var products = from Product in db.Product.Where(products => products.Speed>=450)
+            //               join pc in db.Pc on products.Model == pc.Model
 
-
-
-
+            //10 /*Найдите модели принтеров, имеющих самую высокую цену. Вывести: model, price */
+            //SELECT DISTINCT model, price FROM Printer
+            //WHERE price = (SELECT MAX(price) FROM Printer)
+            //var printers = db.Printer
+            //int maxPrice = db.Printer.Max(max => max.Price);
+            //select new
+            //{
+            //    model = printers.Model,
+            //    price = maxPrice,
+            //}
 
         }
     }
